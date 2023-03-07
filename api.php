84764ddbe86dc8b9e06ab2ab83fda9c7548ed0c8
gfftr <gff.terl@gmail.com>
2023-03-07 22:40:47 +0000

    first commit

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+
+// function query($query)
+// {
+//  $res = false;
+//  if ($con = mysqli_connect('localhost', 'root', '')) {
+//   die("unable to connect");
+//  }
+
+//  $result = mysqli_query($con, $query);
+
+//  if ($result && mysqli_num_rows($result) > 0) {
+//   while ($row = mysqli_fetch_assoc($result)) {
+//    //
+//    $res[] = $row;
+//   }
+//  }
+
+//  return $res;
+// }
